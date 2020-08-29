678ad2c2a6db4bb8845d9711c144db062bcd4760
felix-hh <50616313+felix-hh@users.noreply.github.com>
2020-08-29 19:05:52 +0200

    Updates

---
+++ precache-manifest.c2a379fe89d49d16356d780cddac2643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ff4b30944cb9105ecfd3cd7345788a",
+    "url": "/CS-Course-Scheduler/index.html"
+  },
+  {
+    "revision": "19e683e001c1da850137",
+    "url": "/CS-Course-Scheduler/static/css/2.f4da3fac.chunk.css"
+  },
+  {
+    "revision": "c2ae410f7cb3c896fd67",
+    "url": "/CS-Course-Scheduler/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "19e683e001c1da850137",
+    "url": "/CS-Course-Scheduler/static/js/2.02f88fd3.chunk.js"
+  },
+  {
+    "revision": "5a114ae867ff83df2282b69de519f831",
+    "url": "/CS-Course-Scheduler/static/js/2.02f88fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ae410f7cb3c896fd67",
+    "url": "/CS-Course-Scheduler/static/js/main.9b125672.chunk.js"
+  },
+  {
+    "revision": "38f796c539a43ca1c2c5",
+    "url": "/CS-Course-Scheduler/static/js/runtime-main.a1ae648d.js"
+  }
+]);
