b7d525d7d7d6a084d7a2b918f9983ab26b7be61c
felix-hh <50616313+felix-hh@users.noreply.github.com>
2020-08-29 18:51:27 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect} from 'react';
 import "rbx/index.css";
-import { Button, Container, Title, Message } from "rbx";
+import { Button, Container, Message, Title } from "rbx";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from './shared/firebase'
-import CourseList from './components/CourseList';
+import firebase from "./shared/firebase"
+import CourseList from "./components/CourseList"
 
-const terms = { F: "Fall", W: "Winter", S: "Spring" };
-const days = ["M", "Tu", "W", "Th", "F"];
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
 const db = firebase.database().ref();
 
-const timeParts = meets => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match
-    ? {}
-    : {
-        days,
-        hours: {
-          start: hh1 * 60 + mm1 * 1,
-          end: hh2 * 60 + mm2 * 1
-        }
-      };
-};
-
-
-const addCourseTimes = course => ({
-  ...course,
-  ...timeParts(course.meets)
-  });  
-
-const addScheduleTimes = schedule => ({
-  title: schedule.title,
-  courses: Object.values(schedule.courses).map(addCourseTimes)
-  });
-
+// AUTH UI
 const uiConfig = {
   signInFlow: 'popup',
   signInOptions: [
@@ -42,22 +17,8 @@ const uiConfig = {
   ],
   callbacks: {
     signInSuccessWithAuthResult: () => false
-  } 
+  }
 };
-
-const Banner = ({ user, title }) => (
-  <React.Fragment>
-    { user ? <Welcome user={ user } /> : <SignIn /> }
-    <Title>{ title || '[loading...]' }</Title>
-  </React.Fragment>
-);
-
-const SignIn = () => (
-  <StyledFirebaseAuth
-    uiConfig={uiConfig}
-    firebaseAuth={firebase.auth()}
-  />
-);
 
 const Welcome = ({ user }) => (
   <Message color="info">
@@ -69,6 +30,41 @@ const Welcome = ({ user }) => (
     </Message.Header>
   </Message>
 );
+
+const SignIn = () => (
+  <StyledFirebaseAuth
+    uiConfig={uiConfig}
+    firebaseAuth={firebase.auth()}
+  />
+);
+
+const Banner = ({ user, title }) => (
+  <React.Fragment>
+    { user ? <Welcome user={ user } /> : <SignIn /> }
+    <Title>{ title || '[loading...]' }</Title>
+  </React.Fragment>
+);
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: Object.values(schedule.courses).map(addCourseTimes)
+});
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
@@ -86,11 +82,12 @@ const App = () => {
     firebase.auth().onAuthStateChanged(setUser);
   }, []);
 
-  return (
+return (
     <Container>
       <Banner title={ schedule.title } user={ user } />
       <CourseList courses={ schedule.courses } user={ user } />
     </Container>
   );
 };
+
 export default App;
